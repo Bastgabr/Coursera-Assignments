c1c474a49f172c9402be0ccb064664e7771290fa
Deepti Agrawal <deeptia@flipkart.com>
2013-04-21 14:38:06 +0530

    func prog week 4

--- Functional Programming Principles in Scala/patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ Functional Programming Principles in Scala/patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -10,8 +10,8 @@ import patmat.Huffman._
 @RunWith(classOf[JUnitRunner])
 class HuffmanSuite extends FunSuite {
   trait TestTrees {
-    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
-    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
   }
 
   test("weight of a larger tree") {
@@ -22,7 +22,7 @@ class HuffmanSuite extends FunSuite {
 
   test("chars of a larger tree") {
     new TestTrees {
-      assert(chars(t2) === List('a','b','d'))
+      assert(chars(t2) === List('a', 'b', 'd'))
     }
   }
 
@@ -30,13 +30,31 @@ class HuffmanSuite extends FunSuite {
     assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
   }
 
+  test("times") {
+    assert(times(List('a', 'b', 'a')).toSet === Set(('a', 2), ('b', 1)))
+  }
+
   test("makeOrderedLeafList for some frequency table") {
-    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
   }
 
   test("combine of some leaf list") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
-    assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+    assert(combine(leaflist) === List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
+  }
+
+  test("decode") {
+    new TestTrees {
+      assert(decode(t2, List[Bit](0, 0)) == List('a'))
+      assert(decode(t2, List[Bit](0, 1)) == List('b'))
+      assert(decode(t2, List[Bit](1)) == List('d'))
+      assert(decode(t1, List[Bit](0)) == List('a'))
+      assert(decode(t1, List[Bit](1)) == List('b'))
+    }
+  }
+  
+  test("decoded secret") {
+    println(decodedSecret)
   }
 
   test("decode and encode a very short text should be identity") {
