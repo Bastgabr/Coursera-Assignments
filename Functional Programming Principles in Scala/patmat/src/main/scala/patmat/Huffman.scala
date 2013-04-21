d2b5e7b086105628d6e2318f45d8a41612359a9c
Deepti Agrawal <deeptia@flipkart.com>
2013-04-21 15:12:35 +0530

    func prog week 4

--- Functional Programming Principles in Scala/patmat/src/main/scala/patmat/Huffman.scala
+++ Functional Programming Principles in Scala/patmat/src/main/scala/patmat/Huffman.scala
@@ -221,7 +221,7 @@ object Huffman {
         case Fork(left, right, ch, wt) if (chars(right).contains(charToEncode)) => 1 :: encodeCharacter(right, charToEncode)
       }
     }
-    
+
     text.flatMap(encodeCharacter(tree, _))
   }
 
@@ -233,7 +233,7 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = table.filter(entry => entry._1 == char).head._2
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -243,14 +243,18 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = {
+    
+  }
 
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -258,5 +262,8 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    val codeTable: CodeTable = convert(tree)
+    text.flatMap(codeBits(codeTable)(_))
+  }
 }
